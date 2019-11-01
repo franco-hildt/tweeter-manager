023a7c5ca25b4f8bfa36835223553ffee65c08b2
franco-hildt <franco.hildt@mercadolibre.com>
2019-11-01 11:40:24 -0300

    Agregando tests

--- src/service/tweet_manager.go
+++ src/service/tweet_manager.go
@@ -6,16 +6,29 @@ import (
 	"github.com/franco-hildt/tweeter-manager/tweeter-manager/src/domain"
 )
 
-var Tweet *domain.Tweet
+var Tweet []domain.Tweet
 
 func PublishTweet(tweet *domain.Tweet) error {
 	if tweet.User == "" {
 		return fmt.Errorf("user is required")
+	} else if len(tweet.Text) > 140 {
+		return fmt.Errorf("text exceeds 140 characters")
+	} else if len(tweet.Text) == 0 {
+		return fmt.Errorf("text is required")
+	} else {
+		Tweet = append(Tweet, *tweet)
 	}
-	Tweet = tweet
 	return nil
 }
 
-func GetTweet() *domain.Tweet {
+func GetTweets() []domain.Tweet {
 	return Tweet
+}
+
+func GetLastTweet() domain.Tweet {
+	return Tweet[len(Tweet)-1]
+}
+
+func InitializeService() {
+	Tweet = make([]domain.Tweet, 0)
 }
--- src/service/tweet_manager_test.go
+++ src/service/tweet_manager_test.go
@@ -12,6 +12,7 @@ import (
 func TestPublishedTweetIsSaved(t *testing.T) {
 
 	// Initialization
+	service.InitializeService()
 	var tweet *domain.Tweet
 	user := "grupoesfera"
 	text := "This is my first tweet"
@@ -21,7 +22,7 @@ func TestPublishedTweetIsSaved(t *testing.T) {
 	service.PublishTweet(tweet)
 
 	// Validation
-	publishedTweet := service.GetTweet()
+	publishedTweet := service.GetLastTweet()
 
 	assert.Equal(t, user, publishedTweet.User, "Should be equal")
 	assert.Equal(t, text, publishedTweet.Text, "Should be equal")
@@ -31,6 +32,7 @@ func TestPublishedTweetIsSaved(t *testing.T) {
 func TestTweetWithoutUserIsNotPublished(t *testing.T) {
 
 	// Initialization
+	service.InitializeService()
 	var tweet *domain.Tweet
 
 	var user string
@@ -47,10 +49,75 @@ func TestTweetWithoutUserIsNotPublished(t *testing.T) {
 	assert.Equal(t, "user is required", err.Error(), "Should be user is required")
 }
 
-func TestTweetWithoutTextIsNotPublished(t *testing.T) {
-	// TO-DO
+func TestCanPublishAndRetrieveMoreThanOneTweet(t *testing.T) {
+	// Initialization
+	service.InitializeService()
+	var tweet, secondTweet *domain.Tweet // Fill the tweets with data
+	text1 := "Primer tweet"
+	text2 := "Segundo tweet"
+	user := "fhildt"
+	tweet = domain.NewTweet(user, text1)
+	secondTweet = domain.NewTweet(user, text2)
+
+	// Operation
+	service.PublishTweet(tweet)
+	service.PublishTweet(secondTweet)
+
+	// Validation
+	publishedTweets := service.GetTweets()
+	assert.Equal(t, 2, len(publishedTweets), "Should be the same len")
+
+	firstPublishedTweet := publishedTweets[0]
+	secondPublishedTweet := publishedTweets[1]
+
+	assert.Equal(t, firstPublishedTweet.Text, text1, "Should be equal")
+	assert.Equal(t, firstPublishedTweet.User, user, "Should be equal")
+
+	assert.Equal(t, secondPublishedTweet.Text, text2, "Should be equal")
+	assert.Equal(t, secondPublishedTweet.User, user, "Should be equal")
+
 }
 
-func TestTweetWichEwxceeding140CharactersIsNotPublished(t *testing.T) {
-	// TO-DO
+func TestTweetWithoutTextIsNotPublished(t *testing.T) {
+
+	// Initialization
+	service.InitializeService()
+	var tweet *domain.Tweet
+
+	user := "grupoesfera"
+	var text string
+
+	tweet = domain.NewTweet(user, text)
+
+	// Operation
+	var err error
+	err = service.PublishTweet(tweet)
+
+	// Validation
+	assert.NotNil(t, err, "Should NOT be nil")
+
+	assert.Equal(t, "text is required", err.Error(), "Should be the same")
+
+}
+
+func TestTweetWhichExceeding140CharactersIsNotPublished(t *testing.T) {
+
+	// Initialization
+	service.InitializeService()
+	var tweet *domain.Tweet
+
+	user := "grupoesfera"
+	text := `The Go project has grown considerably with over half a million users and community members 
+	all over the world. To date all community oriented activities have been organized by the community
+	with minimal involvement from the Go project. We greatly appreciate these efforts`
+
+	tweet = domain.NewTweet(user, text)
+
+	// Operation
+	var err error
+	err = service.PublishTweet(tweet)
+
+	// Validation
+	assert.NotNil(t, err, "Should NOT be nil")
+	assert.Equal(t, "text exceeds 140 characters", err.Error(), "Should be equal")
 }
